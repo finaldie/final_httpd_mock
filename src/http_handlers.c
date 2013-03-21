bd9b77975a8d3c804c65a2aab878b689f0b2a6b8
huyuzhang <hyzwowtools@gmail.com>
2013-03-21 10:37:18 +0800

    Refactor code, one response type one file

--- src/http_handlers.c
+++ src/http_handlers.c
@@ -16,36 +16,9 @@
 
 #include "http_handlers.h"
 
-#define fake_response_header \
-"HTTP/1.1 200 OK\r\n" \
-"Date: Tue, 13 Nov 2012 13:21:30 GMT\r\n" \
-"Server: Http Mock\r\n" \
-"Last-Modified: Tue, 12 Jan 2010 13:48:00 GMT\r\n" \
-"Content-Length: %d\r\n" \
-"Connection: Keep-Alive\r\n" \
-"Content-Type: text/html\r\n" \
-"\r\n"
-
-#define fake_chunk_response_header \
-"HTTP/1.1 200 OK\r\n" \
-"Date: Tue, 13 Nov 2012 13:21:30 GMT\r\n" \
-"Server: Http Mock\r\n" \
-"Last-Modified: Tue, 12 Jan 2010 13:48:00 GMT\r\n" \
-"Transfer-Encoding: chunked\r\n" \
-"Connection: Keep-Alive\r\n" \
-"Content-Type: text/html\r\n" \
-"\r\n"
-
-#define fake_response_body "%s\r\n"
-
-#define FHTTP_REPONSE_HEADER_SIZE (sizeof(fake_response_header) + 10 )
-#define FHTTP_CRLF                "\r\n"
-#define FHTTP_CRLF_SIZE           (sizeof(FHTTP_CRLF) - 1)
-#define FHTTP_1MS                 (1000000l)
-#define FHTTP_CHUNK_END           "0\r\n\r\n"
-#define FHTTP_CHUNK_END_SIZE      (sizeof(FHTTP_CHUNK_END) - 1)
-#define FHTTP_CHUNK_RESPONSE_HEADER_SIZE (sizeof(fake_chunk_response_header) - 1)
+#define FHTTP_REPONSE_HEADER_SIZE        2048
 #define FHTTP_MAX_WAIT_TIME_FOR_1ST_REQ  (1000 * 10)
+#define FHTTP_1MS                        (1000000l)
 
 /**
  * design mode:
@@ -56,56 +29,13 @@
  * timer  timer  timer
  */
 
-struct client;
-struct timer_mgr;
-struct client_mgr;
-
-typedef struct timer_node {
-    struct client*     cli;
-    struct timer_node* prev;
-    struct timer_node* next;
-    struct timer_mgr*  owner;
-    int                timeout; // unit [ms]
-} timer_node;
-
-typedef struct client {
-    int         fd;
-    int         offset;
-    int         request_complete;
-    int         response_complete;
-    my_time     last_active;
-    fev_buff*   evbuff;
-    timer_node* tnidx;
-    struct client_mgr* owner;
-
-    int         ischunked;
-    int         chunk_block_num;
-    int         chunk_size;
-    int         last_data_size;
-} client;
-
-typedef struct timer_mgr {
-    timer_node* head;
-    timer_node* tail;
-    int         count;
-} timer_mgr;
-
-typedef struct client_mgr {
-    timer_mgr  tm_main;
-    timer_mgr  tm_minor;
-    timer_mgr* current;
-    timer_mgr* backup;
-
-    service_arg_t* sargs;
-    int        current_conn;
-    size_t     buffsize;
-    char*      response_buf;
-    char*      response_body_buf;
-} client_mgr;
-
 static fev_state* fev = NULL;
 static client_mgr* cli_mgr = NULL;
 extern log_file_t* glog;
+
+struct resp_tbl_t {
+   register_resp_init init;
+} resp_tbl[RESP_TYPE_NUM];
 
 static
 timer_node* timer_node_create(client* cli, int timeout)
@@ -208,6 +138,7 @@ void destroy_client(client* cli)
     int fd = fevbuff_destroy(cli->evbuff);
     timer_node_delete(cli->owner->current, cli->tnidx);
     cli->owner->current_conn--;
+    cli->opt.free(cli);
     free(cli);
     close(fd);
     FLOG_DEBUG(glog, "destroy client fd=%d", fd);
@@ -240,155 +171,14 @@ int gen_number_in_range(int min, int max)
     return min + offset;
 }
 
-static
 int gen_random_response_size(int min, int max)
 {
     return gen_number_in_range(min, max);
 }
 
-static
-int get_ischunked(resp_type_t response_type, int chunk_ratio)
-{
-    if ( response_type == RESP_TYPE_CONTENT ) {
-        return 0;
-    } else if ( response_type == RESP_TYPE_CHUNKED ) {
-        return 1;
-    } else {
-        if ( chunk_ratio == 0 ) {
-            return 0;
-        } else if ( chunk_ratio == 100 ) {
-            return 1;
-        } else {
-            return (rand() % 100) < chunk_ratio;
-        }
-    }
-}
-
-static
 int gen_random_latency(int min, int max)
 {
     return gen_number_in_range(min, max);
-}
-
-static
-int create_response(char* buf, size_t buffsize, size_t size)
-{
-    if ( size > (buffsize - 3) ) return 0;
-
-    // fill all bytes with 'F'
-    memset(buf, 70, size);
-    buf[size] = '\r';
-    buf[size+1] = '\n';
-    buf[size+2] = '\0';
-
-    return size + FHTTP_CRLF_SIZE;
-}
-
-static
-int send_http_response(client* cli, int* need_shutdown)
-{
-    // here we have two options to implement
-    // 1. snprintf once ( we choose this )
-    // 2. writev ( in future )
-
-    client_mgr* mgr = cli->owner;
-    *need_shutdown = cli->owner->sargs->timeout ? 0 : 1;
-
-    // 1. generate response body
-    int response_size = gen_random_response_size(mgr->sargs->min_response_size,
-                                                 mgr->sargs->max_response_size);
-    create_response(mgr->response_body_buf, mgr->buffsize, response_size);
-    // 2. fill whole response
-    int total_len = snprintf(mgr->response_buf, mgr->buffsize,
-                             fake_response_header fake_response_body,
-                             response_size + (int)FHTTP_CRLF_SIZE * 2,
-                             mgr->response_body_buf);
-    // 3. send out
-    cli->response_complete++;
-    return fevbuff_write(cli->evbuff, mgr->response_buf, total_len);
-}
-
-// create simple chunk
-// follow the chunk format:
-// normal data:
-//  size CRLF
-//  data CRLF
-// end data:
-//  0 CRLF
-static
-size_t create_chunk_response(char* buf, size_t buffsize, size_t datasize)
-{
-    int offset = snprintf(buf, buffsize, "%lx\r\n", datasize);
-    memset(buf+offset, 70, datasize);
-    offset += datasize;
-    buf[offset] = '\r';
-    buf[offset+1] = '\n';
-    buf[offset+2] = '\0';
-
-    size_t totalsize = offset + FHTTP_CRLF_SIZE;
-    return totalsize;
-}
-
-static
-int set_chunked_status(client* cli)
-{
-    if ( !cli ) return 1;
-
-    client_mgr* mgr = cli->owner;
-    // 1. generate response body
-    int response_size = gen_random_response_size(mgr->sargs->min_chunk_response_size,
-                                                 mgr->sargs->max_chunk_response_size);
-    cli->last_data_size = response_size;
-    cli->chunk_size = response_size / mgr->sargs->chunk_blocks;
-    // fix zero chunk size issue
-    if ( cli->last_data_size && !cli->chunk_size ) {
-        cli->chunk_size = 1;
-    }
-
-    return 0;
-}
-
-static
-int send_http_chunked_response(client* cli, int* need_shutdown)
-{
-    client_mgr* mgr = cli->owner;
-    int offset = 0;
-    *need_shutdown = 0;
-
-    // if this is the first chunk block, fill response headers first
-    if ( !cli->chunk_block_num ) {
-        // first time to send, construct header
-        memcpy(mgr->response_buf, fake_chunk_response_header,
-                FHTTP_CHUNK_RESPONSE_HEADER_SIZE);
-        offset += FHTTP_CHUNK_RESPONSE_HEADER_SIZE;
-    }
-
-    // fill response
-    if ( !cli->last_data_size ) {
-        // if no data left, fill the last chunk
-        memcpy(&mgr->response_buf[offset], FHTTP_CHUNK_END, FHTTP_CHUNK_END_SIZE);
-        offset += FHTTP_CHUNK_END_SIZE;
-
-        // mark response complete and reset block num for next request
-        cli->response_complete++;
-        cli->chunk_block_num = 0;
-        *need_shutdown = mgr->sargs->timeout ? 0 : 1;
-    } else {
-        // have data left, fill normal chunk
-        int datasize = cli->chunk_size < cli->last_data_size ? cli->chunk_size :
-                                                               cli->last_data_size;
-        int len = create_chunk_response(mgr->response_buf + offset,
-                                        mgr->buffsize - offset,
-                                        datasize);
-        // update status
-        cli->last_data_size -= datasize;
-        cli->chunk_block_num++;
-
-        offset += len;
-    }
-
-    // send out
-    return fevbuff_write(cli->evbuff, mgr->response_buf, offset);
 }
 
 static
@@ -405,17 +195,13 @@ void http_on_timer(fev_state* fev, void* arg)
     while ( node ) {
         int diff = get_diff_time(&node->cli->last_active, &now) / 1000;
         int fd = node->cli->fd;
-        FLOG_DEBUG(glog, "on timer: fd=%d, diff=%d", fd, diff);
+        FLOG_DEBUG(glog, "on timer: fd=%d, diff=%d, node_timeout=%d", fd, diff, node->timeout);
 
         if ( node->cli->response_complete < node->cli->request_complete ) {
             if ( diff >= node->timeout ) {
                 // we need to send respose
-                int ret = -1, need_shutdown = 0;
-                if ( !node->cli->ischunked ) {
-                    ret = send_http_response(node->cli, &need_shutdown);
-                } else {
-                    ret = send_http_chunked_response(node->cli, &need_shutdown);
-                }
+                int ret = -1, complete = 0;
+                ret = node->cli->opt.handler(node->cli, &complete);
 
                 if ( ret < 0 ) {
                     // something goes wrong, the client has been destroyed, go
@@ -424,17 +210,24 @@ void http_on_timer(fev_state* fev, void* arg)
                     goto pop_next_node;
                 }
 
+                // update last active
+                get_cur_time(&node->cli->last_active);
+
                 // when we found the server timeout == 0, we can fast shutdown
                 // the connection instead of going to next timer round checking
-                if ( need_shutdown ) {
-                    destroy_client(node->cli);
-                    FLOG_DEBUG(glog, "on timer, timeout==0 fast shutdown, fd=%d", fd);
-                    goto pop_next_node;
+                if ( complete ) {
+                    if( mgr->sargs->timeout == 0 ) {
+                        destroy_client(node->cli);
+                        FLOG_DEBUG(glog, "on timer, timeout==0 fast shutdown, fd=%d", fd);
+                        goto pop_next_node;
+                    } else {
+                        // we are finished one request, then reset timeout
+                        node->timeout = mgr->sargs->timeout;
+                        node->cli->response_complete++;
+        	        FLOG_DEBUG(glog, "on timer: fd=%d, we have finished a request, reset timeout to %d",
+                                   fd, mgr->sargs->timeout);
+                    }
                 }
-
-                // we are finished one request, then reset timeout and last active time
-                get_cur_time(&node->cli->last_active);
-                node->timeout = mgr->sargs->timeout;
             }
 
             timer_node_push(mgr->backup, node);
@@ -502,40 +295,17 @@ void http_read(fev_state* fev, fev_buff* evbuff, void* arg)
     // check k-a valid
     if ( (cli->request_complete - 1) != cli->response_complete ) {
         destroy_client(cli);
-        FLOG_ERROR(glog, "not follow keep-alive rules, check client side");
+        FLOG_ERROR(glog, "Didn't follow the keep-alive rules, check client side, request_cl_cnt=%d, response_cl_cnt=%d",
+                   cli->request_complete, cli->response_complete);
         return;
     }
 
     // mark active
     get_cur_time(&cli->last_active);
 
-    // to decide this response whether is chunked
-    cli->ischunked = get_ischunked(cli->owner->sargs->response_type,
-                                    cli->owner->sargs->chunk_ratio);
-
-    int interval = 0, ret = 0, need_shutdown = 0;
-    if ( !cli->ischunked ) {
-        int latency = gen_random_latency(cli->owner->sargs->min_latency,
-                                     cli->owner->sargs->max_latency);
-        if ( latency ) {
-            interval = latency;
-        } else {
-            interval = cli->owner->sargs->timeout;
-            ret = send_http_response(cli, &need_shutdown);
-        }
-    } else {
-        // set chunk status if client need chunked response
-        set_chunked_status(cli);
-
-        int latency = gen_random_latency(cli->owner->sargs->min_chunk_latency,
-                                     cli->owner->sargs->max_chunk_latency);
-        if ( latency ) {
-            interval = latency;
-        } else {
-            interval = cli->owner->sargs->timeout;
-            ret = send_http_chunked_response(cli, &need_shutdown);
-        }
-    }
+    int ret = 0, complete = 0;
+    cli->opt.pre_send_req(cli);
+    ret = cli->opt.handler(cli, &complete);
 
     if ( ret < 0 ) {
         // something goes wrong, client has been destroyed
@@ -543,14 +313,23 @@ void http_read(fev_state* fev, fev_buff* evbuff, void* arg)
         return;
     }
 
-    if ( need_shutdown ) {
-        destroy_client(cli);
-        FLOG_DEBUG(glog, "timeout==0 fast shutdown, fd=%d", fd);
-        return;
+    int latency = 0;
+    if ( complete ) {
+        int server_timeout = cli->owner->sargs->timeout;
+        if( server_timeout == 0 ) {
+            destroy_client(cli);
+            FLOG_DEBUG(glog, "timeout==0 fast shutdown, fd=%d", fd);
+            return;
+        } else {
+            latency = server_timeout;
+            cli->response_complete++;
+        }
+    } else {
+        latency = cli->opt.get_latency(cli);
     }
 
     // create timer node
-    timer_node* tnode = timer_node_create(cli, interval);
+    timer_node* tnode = timer_node_create(cli, latency);
     timer_node_push(cli->owner->current, tnode);
 
     // pop last consumed data
@@ -584,9 +363,12 @@ void http_accept(fev_state* fev, int fd, void* ud)
         cli->evbuff = evbuff;
         cli->owner = mgr;
         cli->owner->current_conn++;
-        cli->ischunked = 0;
         timer_node* tnode = timer_node_create(cli, FHTTP_MAX_WAIT_TIME_FOR_1ST_REQ);
         timer_node_push(cli->owner->current, tnode);
+        // init client private opt
+        resp_tbl[mgr->sargs->response_type].init(cli);
+        // call client's init
+        cli->opt.init(cli);
 
         FLOG_DEBUG(glog, "fev_buff created fd=%d", fd);
     } else {
@@ -597,11 +379,25 @@ EG_ERROR:
 }
 
 static
-void http_on_show_status(fev_state* fev, void* arg)
+void http_show_status(fev_state* fev, void* arg)
 {
     client_mgr* mgr = (client_mgr*)arg;
 
     FLOG_INFO(glog, "current connection = %d", mgr->current_conn);
+}
+
+static
+void register_resp(resp_type_t type, register_resp_init init)
+{
+    resp_tbl[type].init = init;   
+}
+
+static
+void register_resp_handlers()
+{
+    register_resp(RESP_TYPE_CONTENT, init_content_resp_opt);
+    register_resp(RESP_TYPE_CHUNKED, init_chunk_resp_opt);
+    register_resp(RESP_TYPE_MIX, init_mix_resp_opt);
 }
 
 int init_listen(service_arg_t* sargs)
@@ -615,6 +411,7 @@ int init_listen(service_arg_t* sargs)
     return 0;
 }
 
+
 int init_service(service_arg_t* sargs)
 {
     fev = fev_create(sargs->max_queue_len);
@@ -624,6 +421,7 @@ int init_service(service_arg_t* sargs)
     }
     FLOG_INFO(glog, "fev create successful");
 
+    register_resp_handlers();
     cli_mgr = create_client_mgr(sargs->max_response_size);
     cli_mgr->sargs = sargs;
 
@@ -642,8 +440,8 @@ int init_service(service_arg_t* sargs)
 
 int start_service()
 {
-    // every 50ms, the timer will wake up
-    fev_timer* resp_timer = fev_add_timer_event(fev, 50 * FHTTP_1MS, 50 * FHTTP_1MS,
+    // every 2ms, the timer will wake up
+    fev_timer* resp_timer = fev_add_timer_event(fev, 10 * FHTTP_1MS, 10 * FHTTP_1MS,
                                                 http_on_timer, cli_mgr);
     if ( !resp_timer ) {
         FLOG_ERROR(glog, "register response timer failed");
@@ -651,7 +449,7 @@ int start_service()
     }
 
     fev_timer* status_timer = fev_add_timer_event(fev, 1000 * FHTTP_1MS, 1000 * FHTTP_1MS,
-                                                http_on_show_status, cli_mgr);
+                                                http_show_status, cli_mgr);
     if ( !status_timer ) {
         FLOG_ERROR(glog, "register status timer failed");
         exit(1);
--- src/http_handlers.h
+++ src/http_handlers.h
@@ -1,12 +1,21 @@
 #ifndef __HTTP_HANDLERS__
 #define __HTTP_HANDLERS__
 
+#include "fev_buff.h"
+#include "fev_timer.h"
+#include "tu_inc.h"
+
 #define FHTTP_MAX_LOG_FILENAME_SIZE 256
+#define FHTTP_CRLF                "\r\n"
+#define FHTTP_CRLF_SIZE           (sizeof(FHTTP_CRLF) - 1)
 
 typedef enum {
     RESP_TYPE_CONTENT = 0,
     RESP_TYPE_CHUNKED,
-    RESP_TYPE_MIX
+    RESP_TYPE_MIX,
+    RESP_PCAP,
+
+    RESP_TYPE_NUM   // do not delete this, count of response type
 } resp_type_t;
 
 typedef struct {
@@ -38,8 +47,71 @@ typedef struct {
     int cpu_cores;
 } service_arg_t;
 
+struct client;
+struct timer_mgr;
+struct client_mgr;
+
+typedef struct timer_node {
+    struct client*     cli;
+    struct timer_node* prev;
+    struct timer_node* next;
+    struct timer_mgr*  owner;
+    int                timeout; // unit [ms]
+} timer_node;
+
+typedef struct timer_mgr {
+    timer_node* head;
+    timer_node* tail;
+    int         count;
+} timer_mgr;
+
+typedef struct response_opt {
+    void (*init)(struct client*);
+    void (*free)(struct client*);
+    int  (*get_latency)(struct client*);
+    void (*pre_send_req)(struct client*);
+    int  (*handler)(struct client*, int* complete);
+} resp_opt;
+
+typedef struct client {
+    int         fd;
+    int         offset;
+    int         request_complete;
+    int         response_complete;
+    my_time     last_active;
+    fev_buff*   evbuff;
+    timer_node* tnidx;
+    struct client_mgr* owner;
+
+    resp_opt    opt;
+    void*       priv;
+} client;
+
+typedef struct client_mgr {
+    timer_mgr  tm_main;
+    timer_mgr  tm_minor;
+    timer_mgr* current;
+    timer_mgr* backup;
+
+    service_arg_t* sargs;
+    int        current_conn;
+    size_t     buffsize;
+    char*      response_buf;
+    char*      response_body_buf;
+} client_mgr;
+
+typedef void (*register_resp_init)(client*);
+
 int init_listen(service_arg_t*);
 int init_service(service_arg_t*);
 int start_service();
+
+int gen_random_response_size(int min, int max);
+int gen_random_latency(int min, int max);
+
+void init_content_resp_opt(client*);
+void init_chunk_resp_opt(client*);
+void init_mix_resp_opt(client*);
+void init_pcap_resp_opt(client*);
 
 #endif
